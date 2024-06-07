9160475800b7461c710f004632ab47134502338f
Darpan3011 <darpankanani301@gmail.com>
2024-06-07 12:26:16 +0530

    Updated the image

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { BsSearch } from 'react-icons/bs';
 import Weather from '../components/Weather';
 import Spinner from '../public/spinner.gif';
+import background from '../public/bg.jpg';
 
 export default function Home() {
   const [city, setCity] = useState('');
@@ -56,13 +57,13 @@ export default function Home() {
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/40 z-[1]' />
       {/* Background image */}
       <Image
-        src='https://images.unsplash.com/photo-1601134467661-3d775b999c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2575&q=80'
+        src={background}
         layout='fill'
         className='object-cover'
       />
 
       {/* City Search */}
-      <div className='flex bg-red-400 md:flex-row flex-col'>
+      <div className='flex md:flex-row flex-col'>
         <div className='relative flex justify-between items-center max-w-[500px] w-full m-auto pt-4 px-4 text-white z-10'>
           <form
             onSubmit={fetchWeatherByCity}
